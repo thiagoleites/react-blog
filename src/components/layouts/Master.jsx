297d7ab33810cf9861055fc54e08f01b043e13f1
Thiago L. Santos <thiagoleites@gmail.com>
2024-03-08 23:45:59 -0300

    feat: add missing elements on home page

--- src/components/layouts/Master.jsx
+++ src/components/layouts/Master.jsx
@@ -5,14 +5,24 @@ import image0 from '../../assets/images/image0.png'
 import image1 from '../../assets/images/image1.png'
 import image2 from '../../assets/images/image2.png'
 import image3 from '../../assets/images/image3.png'
+import blog1 from '../../assets/images/blog1.png'
+import blog2 from '../../assets/images/blog2.png'
+import blog3 from '../../assets/images/blog3.png'
+import blog4 from '../../assets/images/blog4.png'
+import blog5 from '../../assets/images/blog5.png'
+import blog6 from '../../assets/images/blog6.png'
 import { ArrowUpRightIcon } from '@heroicons/react/24/solid'
-import BlogPost from '../common/BlogPost.jsx'
+// import BlogPost from '../common/BlogPost.jsx'
 
 const tagColors = {
   Design: '#6941C6',
   Research: '#3538CD',
   Presentation: '#C11574',
-  Interface: '#C11574'
+  Interface: '#C11574',
+  Leadership: '#6941C6',
+  Management: '#363F72',
+  Frameworks: '#C4320A',
+  Product: '#026AA2'
 }
 
 function rendereizarTags (tags, limite) {
@@ -153,6 +163,90 @@ function Master () {
                                         {rendereizarTags(['Design', 'Interface'], 2)}
                                     </div>
                                 </div>
+                            </article>
+
+                        </div>
+                    </section>
+
+                    <section className='posts'>
+                        <h2>All blog posts</h2>
+                        <div className="posts_blogs">
+                            <article className="post_item">
+                                <figure>
+                                    <img src={blog1} alt="Simple article blog" />
+                                </figure>
+                                <span>Alec Whitten • 1 Jan 2023</span>
+                                <div className="post_title_icon">
+                                    <h2>Bill Walsh leadership lessons</h2>
+                                    <ArrowUpRightIcon className='post_item_icon' />
+                                </div>
+                                <p>Like to know the secrets of transforming a 2-14 team into a 3x Super Bowl winning Dynasty?</p>
+                                {rendereizarTags(['Leadership', 'Management'], 2)}
+                            </article>
+
+                            <article className="post_item">
+                                <figure>
+                                    <img src={blog2} alt="Simple article blog" />
+                                </figure>
+                                <span>Demi Wllkinson • 1 Jan 2023</span>
+                                <div className="post_title_icon">
+                                    <h2>PM mental models</h2>
+                                    <ArrowUpRightIcon className='post_item_icon' />
+                                </div>
+                                <p>l models are simple expressions of complex processes or relationships.</p>
+                                {rendereizarTags(['Product', 'Research', 'Frameworks'], 3)}
+                            </article>
+
+                            <article className="post_item">
+                                <figure>
+                                    <img src={blog3} alt="Simple article blog" />
+                                </figure>
+                                <span>Candice Wu • 1 Jan 2023</span>
+                                <div className="post_title_icon">
+                                    <h2>What is Wireframming?</h2>
+                                    <ArrowUpRightIcon className='post_item_icon' />
+                                </div>
+                                <p>Introduction to Wireframing and its Principles. Learn from the best in the indeustry.</p>
+                                {rendereizarTags(['Design', 'Research'], 2)}
+                            </article>
+
+                            <article className="post_item">
+                                <figure>
+                                    <img src={blog4} alt="Simple article blog" />
+                                </figure>
+                                <span>Candice Wu • 1 Jan 2023</span>
+                                <div className="post_title_icon">
+                                    <h2>How collaboration makes us better designers</h2>
+                                    <ArrowUpRightIcon className='post_item_icon' />
+                                </div>
+                                <p>Collaboration can make our teams stronger, and our individual designs better.</p>
+                                {rendereizarTags(['Design', 'Research'], 2)}
+                            </article>
+
+                            <article className="post_item">
+                                <figure>
+                                    <img src={blog5} alt="Simple article blog" />
+                                </figure>
+                                <span>Drew Cano • 1 Jan 2023</span>
+                                <div className="post_title_icon">
+                                    <h2>Our top 10 Javascript frameworks to use</h2>
+                                    <ArrowUpRightIcon className='post_item_icon' />
+                                </div>
+                                <p>JavaScript frameworks make development easy with extensive features and functionalities.</p>
+                                {rendereizarTags(['Design', 'Research'], 2)}
+                            </article>
+
+                            <article className="post_item">
+                                <figure>
+                                    <img src={blog6} alt="Simple article blog" />
+                                </figure>
+                                <span>Orlando Diggs • 1 Jan 2023</span>
+                                <div className="post_title_icon">
+                                    <h2>Podecast: Creating a better CX Community</h2>
+                                    <ArrowUpRightIcon className='post_item_icon' />
+                                </div>
+                                <p>Startomg a community doesn't need to be complicated, but how do you get started?</p>
+                                {rendereizarTags(['Design', 'Research'], 2)}
                             </article>
 
                         </div>
